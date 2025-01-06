dff5d9e09ef2f24de783364447daf307f3ba65ac
SuminJN <wjstnals1211@gmail.com>
2025-01-06 16:03:14 +0900

    타임리프 html 추가

--- src/main/java/hello/coupang_clone/domain/Item.java
+++ src/main/java/hello/coupang_clone/domain/Item.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public class Item {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
